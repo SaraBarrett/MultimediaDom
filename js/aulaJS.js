966ce4b76dbe5bf8ccc6ff16a16874c385284de8
SaraBarrett <sara.c.mon@gmail.com>
2025-07-09 12:05:42 +0100

    DOM

--- js/aulaJS.js
+++ js/aulaJS.js
@@ -4,5 +4,65 @@ function changeStyle() {
   document.querySelector("h1").style.color = "white";
 }
 
+// let myImage = document.getElementById('unicorn');
+// let myH1 = document.getElementById('mainheading');
 
+// console.log(myImage)
+// console.log(myH1)
 
+//let doneTodos = document.querySelectorAll('.done')
+
+// let doneTodos = document.getElementsByClassName('done')
+// console.log(doneTodos)
+
+// let checkbox = document.querySelector("input[type='checkbox']");
+// //let checkbox = document.getElementById('my-input');
+// console.log(checkbox)
+
+function changeImg() {
+  //seleccionar elemento
+  let myImage = document.querySelector("img");
+
+  if (myImage.src == "https://devsprouthosting.com/images/chicken.jpg") {
+    //manipular elemento
+    myImage.src = "https://devsprouthosting.com/images/egg.jpg";
+  } else {
+    //manipular elemento
+    myImage.src = "https://devsprouthosting.com/images/chicken.jpg";
+  }
+}
+
+function changeTaste() {
+  let pickles = document.querySelector("span");
+  pickles.innerText = "yack";
+}
+
+function rainbow() {
+  const colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+  ];
+
+  //seleccionar todos os elementos span
+
+  let mySpans = document.getElementsByTagName("span");
+
+  let contador = 0;
+  for(let element of mySpans){
+      element.style.color = colors[contador];
+      contador = contador +1;
+  }
+
+  //no elemento [0] da lista de span colocar colors[0]
+  //no elemento [1] da lista de span colocar colors[1]
+  //no elemento [2] da lista de span colocar colors[2]
+  //..
+
+  //ajuda
+  //element.style.backgroundColor = colors[2];
+}
--- js/script.js
+++ js/script.js
@@ -1,2 +1,91 @@
-let myImagem = document.getElementById('banner');
-console.log(myImagem)
+// let myImagem = document.getElementById('banner');
+// console.log(myImagem)
+
+// let myPs = document.getElementsByTagName("p");
+// console.log(myPs);
+
+//vai buscar todos os elemntos com a class square
+// let myImages = document.getElementsByClassName("square");
+// console.log(myImages);
+
+// //vai buscar o primeiro elemento da classe .square
+// let mySqImg = document.querySelector(".square");
+// console.log(mySqImg);
+
+// //vai buscar todos os elementos da tag img
+// let mySqImgA = document.querySelectorAll("img");
+// console.log(mySqImgA);
+
+function translatePT() {
+  //seleccção
+  let myH1 = document.querySelector("h1");
+  let myContent = document.getElementById("mw-toc-heading");
+
+  //manipular o texto
+  myH1.innerText = "Galinhas de Seda";
+  myContent.innerText = "Conteúdos";
+}
+
+function changeAtt() {
+  let myImageBanner = document.getElementById("banner");
+  myImageBanner.src =
+    "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia11604/a-galinha-domestica-e-suas-principais-racas-comerciais-cpt11.jpg";
+}
+
+function darkMode() {
+  let body = document.querySelector("body");
+  let myPs = document.querySelectorAll("p");
+
+  if (body.style.backgroundColor == "white") {
+    body.style.backgroundColor = "black";
+
+    for (let element of myPs) {
+      element.style.color = "white";
+    }
+  } else {
+    body.style.backgroundColor = "white";
+
+    for (let element of myPs) {
+      element.style.color = "black";
+    }
+  }
+}
+
+function changeLisBG(){
+  //selecionar
+  let myLis = document.getElementsByClassName('toclevel-1');
+  console.log(myLis)
+
+
+  //alternativa 
+  //document.querySelectorAll('.toclevel-1');
+  //manipular
+  for(let element of myLis){
+    
+    if( element.style.backgroundColor == "green"){
+      element.style.backgroundColor = "white";
+    }else{
+      element.style.backgroundColor = "green";
+    }
+  }
+
+}
+
+
+function login(){
+  //criar o elemento p e img
+  const newP = document.createElement('p');
+  const newImage = document.createElement('img');
+
+  //no novo paragrafo definir o texto
+  newP.innerText = 'Olá Sara';
+  newImage.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/250px-User_icon_2.svg.png';
+
+  //seleccionar o elemento pai
+  let myParent = document.getElementById('myParent');
+
+  //dar o elemento criado como filho da div
+  myParent.appendChild(newP);
+  myParent.appendChild(newImage);
+
+}
